323f1faa2400cf83bbfb1b7ca0e83c4cfd8a00de
SoluiNet <dennis@grosse-venhaus.net>
2019-02-06 17:59:57 +0100

    add new user controls

---
+++ SoluiNet.DevTools.UI/UserControls/ConnectionString.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoluiNet.DevTools.UI.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für ConnectionString.xaml
+    /// </summary>
+    public partial class ConnectionString : UserControl
+    {
+        public ConnectionString()
+        {
+            InitializeComponent();
+        }
+
+        private bool Expanded { get; set; }
+
+        public string Value
+        {
+            get { return ConnectionStringValue.Text; }
+            set { ConnectionStringValue.Text = value; }
+        }
+
+        public string NameKey
+        {
+            get { return ConnectionStringName.Text; }
+            set { ConnectionStringName.Text = value; }
+        }
+
+        public string ProviderName { get; set; }
+
+        private void AdditionalInfo_Click(object sender, RoutedEventArgs e)
+        {
+            if (!this.Expanded)
+            {
+                this.Expanded = true;
+
+                AdditionalInfo.Content = "^";
+                this.Height = 200;
+
+                var providerNameTextBox = new TextBox()
+                {
+                    Margin = new Thickness(10, 45, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "ProviderName"
+                };
+
+                providerNameTextBox.Text = ProviderName;
+                providerNameTextBox.TextChanged += (o, args) => { this.ProviderName = ((TextBox) o).Text; };
+
+                ContentGrid.Children.Add(providerNameTextBox);
+            }
+            else
+            {
+                AdditionalInfo.Content = "...";
+                this.Height = 45;
+
+                var providerNameTextBox = ContentGrid.FindName("ProviderName") as UIElement;
+                ContentGrid.Children.Remove(providerNameTextBox);
+
+                this.Expanded = false;
+            }
+        }
+    }
+}
---
+++ SoluiNet.DevTools.UI/UserControls/ManageConnectionStrings.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoluiNet.DevTools.UI.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für ManageConnectionStrings.xaml
+    /// </summary>
+    public partial class ManageConnectionStrings : UserControl
+    {
+        public ManageConnectionStrings()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadConnectionStrings()
+        {
+            foreach (ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings)
+            {
+                MainContent.RowDefinitions.Add(new RowDefinition());
+
+                var connectionStringView = new ConnectionString();
+                connectionStringView.Value = connectionString.ConnectionString;
+                connectionStringView.NameKey = connectionString.Name;
+                connectionStringView.ProviderName = connectionString.ProviderName;
+
+                MainContent.Children.Add(connectionStringView);
+                Grid.SetRow(connectionStringView, MainContent.RowDefinitions.Count - 1);
+            }
+        }
+
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+            LoadConnectionStrings();
+        }
+
+        private void ReloadConnectionStrings_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Children.Clear();
+            MainContent.RowDefinitions.Clear();
+
+            ConfigurationManager.RefreshSection("connectionStrings");
+
+            LoadConnectionStrings();
+        }
+
+        private void SaveConnectionStrings_Click(object sender, RoutedEventArgs e)
+        {
+            var data = string.Empty;
+            data += "<connectionStrings>";
+
+            foreach (var item in MainContent.Children)
+            {
+                if (!(item is ConnectionString))
+                {
+                    continue;
+                }
+
+                var connectionString = (ConnectionString) item;
+
+                if (connectionString.ConnectionStringName.Text == "LocalSqlServer")
+                {
+                    continue;
+                }
+
+                data += string.Format(@"<add name=""{0}"" connectionString=""{1}"" providerName=""{2}"" />",
+                    connectionString.NameKey, 
+                    connectionString.Value,
+                    connectionString.ProviderName);
+            }
+
+            data += "</connectionStrings>";
+
+            File.Move("./ConnectionStrings.config", string.Format("./ConnectionStrings.{0:yyyy-MM-ddTHH-mm-ss}.config", DateTime.Now));
+
+            File.WriteAllText("./ConnectionStrings.config", data);
+
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+    }
+}
