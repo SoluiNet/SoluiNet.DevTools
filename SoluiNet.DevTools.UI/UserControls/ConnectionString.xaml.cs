a45a5bfe13dcc67d09ebd292450543f3397fd4e8
SoluiNet <dennis@grosse-venhaus.net>
2019-02-06 19:02:44 +0100

    update extended display of connection strings

--- SoluiNet.DevTools.UI/UserControls/ConnectionString.xaml.cs
+++ SoluiNet.DevTools.UI/UserControls/ConnectionString.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -41,6 +43,11 @@ namespace SoluiNet.DevTools.UI.UserControls
 
         public string ProviderName { get; set; }
 
+        public string Environment
+        {
+            get { return NameKey.Contains(".") ? NameKey.Substring(NameKey.LastIndexOf(".", StringComparison.InvariantCulture) + 1) : "Default"; }
+        }
+
         private void AdditionalInfo_Click(object sender, RoutedEventArgs e)
         {
             if (!this.Expanded)
@@ -50,18 +57,168 @@ namespace SoluiNet.DevTools.UI.UserControls
                 AdditionalInfo.Content = "^";
                 this.Height = 200;
 
+                #region provider name
                 var providerNameTextBox = new TextBox()
                 {
                     Margin = new Thickness(10, 45, 0, 0),
                     HorizontalAlignment = HorizontalAlignment.Left,
                     VerticalAlignment = VerticalAlignment.Top,
-                    Name = "ProviderName"
+                    Name = "ProviderName",
+                    Width = 200
                 };
 
                 providerNameTextBox.Text = ProviderName;
-                providerNameTextBox.TextChanged += (o, args) => { this.ProviderName = ((TextBox) o).Text; };
+                providerNameTextBox.TextChanged += (o, args) => { this.ProviderName = ((TextBox)o).Text; };
 
                 ContentGrid.Children.Add(providerNameTextBox);
+                #endregion provider name
+
+                #region environment
+                var environmentTextBox = new TextBox()
+                {
+                    Margin = new Thickness(215, 45, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "Environment",
+                    Width = 200
+                };
+
+                environmentTextBox.Text = Environment;
+
+                ContentGrid.Children.Add(environmentTextBox);
+                #endregion
+
+                #region server
+                var serverTextBox = new TextBox()
+                {
+                    Margin = new Thickness(10, 70, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "Server",
+                    Width = 200
+                };
+
+                var dataSourceRegex = new Regex("data source=(.+?);", RegexOptions.IgnoreCase);
+
+                var dataSourceMatch = dataSourceRegex.Match(Value);
+
+                if (dataSourceMatch.Success)
+                    serverTextBox.Text = dataSourceMatch.Groups[1].Value;
+
+                ContentGrid.Children.Add(serverTextBox);
+                #endregion
+
+                #region database
+                var databaseTextBox = new TextBox()
+                {
+                    Margin = new Thickness(215, 70, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "Database",
+                    Width = 200
+                };
+
+                var databaseRegex = new Regex("initial catalog=(.+?);", RegexOptions.IgnoreCase);
+
+                var databaseMatch = databaseRegex.Match(Value);
+
+                if (databaseMatch.Success)
+                    databaseTextBox.Text = databaseMatch.Groups[1].Value;
+
+                ContentGrid.Children.Add(databaseTextBox);
+                #endregion
+
+                #region username
+                var usernameTextBox = new TextBox()
+                {
+                    Margin = new Thickness(10, 95, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "Username",
+                    Width = 200
+                };
+
+                var usernameRegex = new Regex("user id=(.+?);", RegexOptions.IgnoreCase);
+
+                var usernameMatch = usernameRegex.Match(Value);
+
+                if (usernameMatch.Success)
+                    usernameTextBox.Text = usernameMatch.Groups[1].Value;
+
+                ContentGrid.Children.Add(usernameTextBox);
+                #endregion
+
+                #region password
+                var passwordTextBox = new TextBox()
+                {
+                    Margin = new Thickness(215, 95, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "Password",
+                    Width = 200
+                };
+
+                var passwordRegex = new Regex("password=(.+?);", RegexOptions.IgnoreCase);
+
+                var passwordMatch = passwordRegex.Match(Value);
+
+                if (passwordMatch.Success)
+                    passwordTextBox.Text = passwordMatch.Groups[1].Value;
+
+                ContentGrid.Children.Add(passwordTextBox);
+                #endregion
+
+                #region use windows authentication
+                var useWindowsAuthenticationTextBox = new CheckBox()
+                {
+                    Margin = new Thickness(10, 120, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "UseWindowsSecurity",
+                    Width = 200
+                };
+
+                var useWindowsAuthenticationRegex = new Regex("integrated security=(.+?);", RegexOptions.IgnoreCase);
+
+                var useWindowsAuthenticationMatch = useWindowsAuthenticationRegex.Match(Value);
+
+                if (useWindowsAuthenticationMatch.Success)
+                    useWindowsAuthenticationTextBox.IsChecked = useWindowsAuthenticationMatch.Groups[1].Value == "SSPI" || Convert.ToBoolean(useWindowsAuthenticationMatch.Groups[1].Value);
+
+                ContentGrid.Children.Add(useWindowsAuthenticationTextBox);
+                #endregion
+
+                #region calculate connectionstring
+                var calculateConnectionStringButton = new Button()
+                {
+                    Margin = new Thickness(420, 45, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Name = "CalculateConnectionString",
+                    Width = 200
+                };
+
+                calculateConnectionStringButton.Content = "Calculate ConnectionString";
+                calculateConnectionStringButton.Click += (o, args) =>
+                {
+                    if (ProviderName == "System.Data.SqlClient")
+                    {
+                        var connectionStringBuilder = new SqlConnectionStringBuilder();
+
+                        connectionStringBuilder.DataSource = (ContentGrid.FindName("Server") as TextBox)?.Text ?? string.Empty;
+                        connectionStringBuilder.InitialCatalog = (ContentGrid.FindName("Database") as TextBox)?.Text ?? string.Empty;
+                        connectionStringBuilder.UserID = (ContentGrid.FindName("Username") as TextBox)?.Text ?? string.Empty;
+                        connectionStringBuilder.Password = (ContentGrid.FindName("Password") as TextBox)?.Text ?? string.Empty;
+                        connectionStringBuilder.IntegratedSecurity = (ContentGrid.FindName("UseWindowsSecurity") as CheckBox)?.IsChecked ?? false;
+
+                        connectionStringBuilder.ApplicationName = "SoluiNet.DevTools";
+
+                        Value = connectionStringBuilder.ConnectionString;
+                    }
+                };
+
+                ContentGrid.Children.Add(calculateConnectionStringButton);
+                #endregion
             }
             else
             {
@@ -70,6 +227,9 @@ namespace SoluiNet.DevTools.UI.UserControls
 
                 var providerNameTextBox = ContentGrid.FindName("ProviderName") as UIElement;
                 ContentGrid.Children.Remove(providerNameTextBox);
+
+                var environmentTextBox = ContentGrid.FindName("Environment") as UIElement;
+                ContentGrid.Children.Remove(environmentTextBox);
 
                 this.Expanded = false;
             }
