4bd31911d3baad3306118ebd7caa7419042b84d2
Dennis Große-Venhaus <dennis@grosse-venhaus.net>
2019-02-17 22:37:40 +0100

    add configuration stub add two new utility plugins

---
+++ SoluiNet.DevTools.Utils.File/FileToolsPlugin.cs
@@ -0,0 +1,28 @@
+﻿using SoluiNet.DevTools.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SoluiNet.DevTools.Utils.File
+{
+    public class FileToolsPlugin : IUtilitiesDevPlugin
+    {
+        public string Name
+        {
+            get { return "FileToolsPlugin"; }
+        }
+
+        public string MenuItemLabel
+        {
+            get { return "File Tools"; }
+        }
+
+        public void Execute(Action<UserControl> displayInPluginContainer)
+        {
+            displayInPluginContainer(new FileToolsUserControl());
+        }
+    }
+}
---
+++ SoluiNet.DevTools.Utils.VsDevEnv/VisualStudioDevEnvToolsPlugin.cs
@@ -0,0 +1,28 @@
+﻿using SoluiNet.DevTools.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SoluiNet.DevTools.Utils.VsDevEnv
+{
+    public class VisualStudioDevEnvToolsPlugin : IUtilitiesDevPlugin
+    {
+        public string Name
+        {
+            get { return "VisualStudioDevEnvToolsPlugin"; }
+        }
+
+        public string MenuItemLabel
+        {
+            get { return "Visual Studio DevEnv Tools"; }
+        }
+
+        public void Execute(Action<UserControl> displayInPluginContainer)
+        {
+            displayInPluginContainer(new VsDevEnvToolsUserControl());
+        }
+    }
+}
