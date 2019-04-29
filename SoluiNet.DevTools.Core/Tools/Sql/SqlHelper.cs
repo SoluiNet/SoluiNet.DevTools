a3508a7ce29ab428bde45b74ebe5a12ae004a9fe
SoluiNet <contact@solui.net>
2019-04-29 21:21:53 +0200

    revert unintended sql helper changes

--- SoluiNet.DevTools.Core/Tools/Sql/SqlHelper.cs
+++ SoluiNet.DevTools.Core/Tools/Sql/SqlHelper.cs
@@ -40,5 +40,96 @@ namespace SoluiNet.DevTools.Core.Tools.Sql
 
             return false;
         }
+
+        public static string GetTableByAlias(string sqlCommand, string alias)
+        {
+            Regex aliasDefinitionRegex = new Regex(string.Format(@"(FROM|JOIN)\s+(.*)\s+(AS\s+)?({0})", alias), RegexOptions.IgnoreCase);
+
+            if (aliasDefinitionRegex.IsMatch(sqlCommand))
+            {
+                var aliasMatch = aliasDefinitionRegex.Match(sqlCommand);
+
+                return aliasMatch.Groups[2].Value;
+            }
+
+            return alias;
+        }
+
+        public static string GetSqlCommandByPosition(string sqlCommand, int cursorPosition)
+        {
+            var beginningOfSqlRegex = new Regex(@"^(\r?\n)?[^\(]?SELECT", RegexOptions.IgnoreCase);
+            var endOfSqlRegex = new Regex(@"((;)|(\r?\n\r?\n))$", RegexOptions.IgnoreCase);
+
+            var sqlCmd = string.Empty;
+
+            sqlCmd += sqlCommand[cursorPosition - 1];
+
+            var beginningOfCommandFound = false;
+            var countOfCharacters = 0;
+
+            while (!beginningOfCommandFound)
+            {
+                if (cursorPosition - 1 <= countOfCharacters)
+                {
+                    return string.Empty;
+                }
+
+                sqlCmd = sqlCommand[cursorPosition - ++countOfCharacters - 1] + sqlCmd;
+
+                if (beginningOfSqlRegex.IsMatch(sqlCmd))
+                {
+                    beginningOfCommandFound = true;
+                }
+            }
+
+            var endOfCommandFound = false;
+            countOfCharacters = 0;
+
+            while (!endOfCommandFound)
+            {
+                if(sqlCommand.Length <= cursorPosition + countOfCharacters)
+                {
+                    return string.Empty;
+                }
+
+                sqlCmd += sqlCommand[cursorPosition + ++countOfCharacters - 1];
+
+                if (endOfSqlRegex.IsMatch(sqlCmd))
+                {
+                    endOfCommandFound = true;
+                }
+            }
+
+            return sqlCmd;
+        }
+
+        public static string GetAliasByPosition(string sqlCommand, int cursorPosition)
+        {
+            var aliasRegex = new Regex("^\\s(([\\\"]?.+[\\\"]?)|(.+))\\.", RegexOptions.IgnoreCase);
+
+            var alias = string.Empty;
+
+            alias += sqlCommand[cursorPosition - 1];
+
+            var aliasFound = false;
+            var countOfCharacters = 0;
+
+            while (!aliasFound)
+            {
+                if (cursorPosition - 1 <= countOfCharacters)
+                {
+                    return string.Empty;
+                }
+
+                alias = sqlCommand[cursorPosition - ++countOfCharacters - 1] + alias;
+
+                if (aliasRegex.IsMatch(alias))
+                {
+                    aliasFound = true;
+                }
+            }
+
+            return alias.Substring(1, alias.Length - 2);
+        }
     }
 }
