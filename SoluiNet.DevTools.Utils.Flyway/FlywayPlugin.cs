0b73fbf9e7f2a90861111d3a7717102a445b93ca
SoluiNet <contact@solui.net>
2019-05-11 13:32:03 +0200

    add flyway supporting plugin

---
+++ SoluiNet.DevTools.Utils.Flyway/FlywayPlugin.cs
@@ -0,0 +1,28 @@
+﻿using SoluiNet.DevTools.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SoluiNet.DevTools.Utils.Flyway
+{
+    public class FlywayPlugin : IUtilitiesDevPlugin
+    {
+        public string Name
+        {
+            get { return "FlywayToolsPlugin"; }
+        }
+
+        public string MenuItemLabel
+        {
+            get { return "Flyway Tools"; }
+        }
+
+        public void Execute(Action<UserControl> displayInPluginContainer)
+        {
+            displayInPluginContainer(new FlywayUserControl());
+        }
+    }
+}
---
+++ SoluiNet.DevTools.Utils.Flyway/FlywayUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoluiNet.DevTools.Utils.Flyway
+{
+    /// <summary>
+    /// Interaktionslogik für FlywayUserControl.xaml
+    /// </summary>
+    public partial class FlywayUserControl : UserControl
+    {
+        public FlywayUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
