24cd6a67b3b4ab4ea5fb1445bdb74d596de4e81f
SoluiNet <contact@solui.net>
2019-05-23 01:40:51 +0200

    fix style cop recommendations for base plugin

--- SoluiNet.DevTools.Core/IBasePlugin.cs
+++ SoluiNet.DevTools.Core/IBasePlugin.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="IBasePlugin.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
 
 namespace SoluiNet.DevTools.Core
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The basis for all SoluiNet.DevTools plugins.
+    /// </summary>
     public interface IBasePlugin
     {
         /// <summary>
-        /// The plugin name
+        /// Gets the plugin name.
         /// </summary>
         string Name { get; }
     }
