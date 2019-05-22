9153e0402ac3173c5cd2c04ad61f8615d81989d5
SoluiNet <contact@solui.net>
2019-05-23 01:39:08 +0200

    add style cop to project

--- SoluiNet.DevTools.Core/Enums/DataExchange/TicketingSystem/SearchIssueEnum.cs
+++ SoluiNet.DevTools.Core/Enums/DataExchange/TicketingSystem/SearchIssueEnum.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="SearchIssueEnum.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
 
 namespace SoluiNet.DevTools.Core.Enums.DataExchange.TicketingSystem
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An enumeration which provides a list of all possible searches in ticketing systems.
+    /// </summary>
     public enum SearchIssueEnum
     {
+        /// <summary>
+        /// All assigned tickets to current user
+        /// </summary>
         AssignedToCurrentUser,
+
+        /// <summary>
+        /// All open assigned tickets to current user
+        /// </summary>
         OpenAssignedToCurrentUser,
+
+        /// <summary>
+        /// All open tickets for a specific project
+        /// </summary>
         OpenForProject,
-        SelectByProject
+
+        /// <summary>
+        /// All tickets for a specific project
+        /// </summary>
+        SelectByProject,
     }
 }
--- SoluiNet.DevTools.Core/Enums/WebClientFormatEnum.cs
+++ SoluiNet.DevTools.Core/Enums/WebClientFormatEnum.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="WebClientFormatEnum.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
 
 namespace SoluiNet.DevTools.Core.Enums
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An enumeration for possible web client formats.
+    /// </summary>
     public enum WebClientFormatEnum
     {
+        /// <summary>
+        /// Working on XML-basis
+        /// </summary>
         Xml = 1,
+
+        /// <summary>
+        /// Working on text-basis
+        /// </summary>
         Text = 2,
-        Binary = 4
+
+        /// <summary>
+        /// Working on binary-basis
+        /// </summary>
+        Binary = 4,
     }
 }
