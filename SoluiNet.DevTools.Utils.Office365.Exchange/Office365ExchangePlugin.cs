011c60cf7517b0a2cbc3b870443954d19f57b235
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-06-24 00:15:26 +0200

    add new plugin for office 365 exchange

---
+++ SoluiNet.DevTools.Utils.Office365.Exchange/Office365ExchangePlugin.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Office365ExchangePlugin.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Utils.Office365.Exchange
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Controls;
+    using SoluiNet.DevTools.Core.Plugin;
+
+    /// <summary>
+    /// A plugin which allows to connect with exchange in an office 365 environment.
+    /// </summary>
+    public class Office365ExchangePlugin : IUtilitiesDevPlugin
+    {
+        /// <summary>
+        /// Gets the technical name of the plugin.
+        /// </summary>
+        public string Name
+        {
+            get { return "Office365ExchangePlugin"; }
+        }
+
+        /// <summary>
+        /// Gets the label for the menu.
+        /// </summary>
+        public string MenuItemLabel
+        {
+            get { return "Office 365 Exchange Tools"; }
+        }
+
+        /// <inheritdoc/>
+        public void Execute(Action<UserControl> displayInPluginContainer)
+        {
+            displayInPluginContainer(new Office365ExchangeUserControl());
+        }
+    }
+}
---
+++ SoluiNet.DevTools.Utils.Office365.Exchange/Office365ExchangeUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Office365ExchangeUserControl.xaml.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Utils.Office365.Exchange
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for Office365ExchangeUserControl.xaml.
+    /// </summary>
+    public partial class Office365ExchangeUserControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Office365ExchangeUserControl"/> class.
+        /// </summary>
+        public Office365ExchangeUserControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
