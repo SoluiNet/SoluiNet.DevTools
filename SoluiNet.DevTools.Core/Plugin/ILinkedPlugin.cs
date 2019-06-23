58f66e3db37253e1708eaf341656e60ff9467248
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-06-23 20:03:03 +0200

    add linked plugins

---
+++ SoluiNet.DevTools.Core/Plugin/ILinkedPlugin.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ILinkedPlugin.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.Plugin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Provides an interface which can be executed in other plugins if supported.
+    /// </summary>
+    public interface ILinkedPlugin
+    {
+    }
+}
---
+++ SoluiNet.DevTools.Core/Plugin/ISupportsLinkedPlugin.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ISupportsLinkedPlugin.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.Plugin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Provides an interface which shows that this plugin supports linked plugins.
+    /// </summary>
+    public interface ISupportsLinkedPlugin
+    {
+    }
+}
