9835adbefd26cb750a05d4626d8758deb60addb4
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-07-18 01:07:36 +0200

    add extension methods for UI XML Data classes

---
+++ SoluiNet.DevTools.Core/UI/XmlData/XmlDataExtension.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="XmlDataExtension.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.UI.XmlData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// A collection of extension methods for xml data classes.
+    /// </summary>
+    public static class XmlDataExtension
+    {
+        /// <summary>
+        /// Convert a SoluiNet.BrushDefinition to <see cref="Brush"/>.
+        /// </summary>
+        /// <param name="soluiNetBrush">The SoluiNet.BrushDefinition.</param>
+        /// <returns>Returns a <see cref="Brush"/> object which represents the overgiven SoluiNet.BrushDefinition.</returns>
+        public static Brush ToBrush(this SoluiNetBrushDefinitionType soluiNetBrush)
+        {
+            Brush brush = null;
+
+            switch (soluiNetBrush.type)
+            {
+                case SoluiNetBrushType.SimpleLinearGradient:
+                    brush = new LinearGradientBrush((Color)ColorConverter.ConvertFromString(soluiNetBrush.startColour), (Color)ColorConverter.ConvertFromString(soluiNetBrush.endColour), Convert.ToDouble(soluiNetBrush.angle));
+                    break;
+                case SoluiNetBrushType.LinearGradient:
+                    brush = new LinearGradientBrush();
+
+                    foreach (var gradientStop in soluiNetBrush.SoluiNetGradientStop)
+                    {
+                        (brush as LinearGradientBrush).GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString(gradientStop.colour), Convert.ToDouble(gradientStop.offset)));
+                    }
+
+                    (brush as LinearGradientBrush).StartPoint = new System.Windows.Point(Convert.ToDouble(soluiNetBrush.SoluiNetStartPoint.xAxis), Convert.ToDouble(soluiNetBrush.SoluiNetStartPoint.yAxis));
+                    (brush as LinearGradientBrush).EndPoint = new System.Windows.Point(Convert.ToDouble(soluiNetBrush.SoluiNetEndPoint.xAxis), Convert.ToDouble(soluiNetBrush.SoluiNetEndPoint.yAxis));
+                    break;
+            }
+
+            return brush;
+        }
+    }
+}
