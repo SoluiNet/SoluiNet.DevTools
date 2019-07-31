2ba394d48126177a5597c0d4a26b1841b4e37bda
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-08-01 00:39:24 +0200

    add new helper class for objects

---
+++ SoluiNet.DevTools.Core/Tools/Object/ObjectHelper.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="ObjectHelper.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.Tools.Object
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A collection of methods to work with objects.
+    /// </summary>
+    public static class ObjectHelper
+    {
+        /// <summary>
+        /// Get a list of primitive properties for the object.
+        /// </summary>
+        /// <param name="instance">The object instance.</param>
+        /// <returns>Returns a <see cref="List{T}"/> of property names.</returns>
+        public static List<string> GetPrimitiveProperties(this object instance)
+        {
+            var instanceType = instance.GetType();
+
+            return instanceType.GetProperties()?.Where(property => property.PropertyType.IsPrimitive || property.PropertyType == typeof(string)).Select(x => x.Name).ToList();
+        }
+
+        /// <summary>
+        /// Get a list of object properties for the object.
+        /// </summary>
+        /// <param name="instance">The object instance.</param>
+        /// <returns>Returns a <see cref="List{T}"/> of property names.</returns>
+        public static List<string> GetObjectProperties(this object instance)
+        {
+            var instanceType = instance.GetType();
+
+            return instanceType.GetProperties()?.Where(property => !property.PropertyType.IsPrimitive && property.PropertyType != typeof(string) && !typeof(ICollection).IsAssignableFrom(property.PropertyType)).Select(x => x.Name).ToList();
+        }
+    }
+}
