326ebf610ea5ecfd909b597e160f8982f0c75a6d
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-08-08 01:42:56 +0200

    update assembly infos

--- SoluiNet.DevTools.SqlPlugin.Example/Properties/AssemblyInfo.cs
+++ SoluiNet.DevTools.SqlPlugin.Example/Properties/AssemblyInfo.cs
@@ -1,18 +1,10 @@
-﻿using System.Reflection;
+﻿// <copyright file="AssemblyInfo.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("SoluiNet.DevTools.SqlPlugin.Example")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SoluiNet")]
-[assembly: AssemblyProduct("SoluiNet.DevTools.SqlPlugin.Example")]
-[assembly: AssemblyCopyright("Copyright © SoluiNet 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
 // für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
@@ -21,16 +13,3 @@ using System.Runtime.InteropServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("563009f9-789e-47d1-b8ab-aa10f1e1eb2a")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// indem Sie "*" wie unten gezeigt eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
