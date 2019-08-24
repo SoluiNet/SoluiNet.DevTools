20c0f40bda60b15c3556e9564193afc296beb4ae
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-08-25 01:50:06 +0200

    Web - make service available for interactive mode

--- SoluiNet.DevTools.Web/Program.cs
+++ SoluiNet.DevTools.Web/Program.cs
@@ -7,6 +7,7 @@ namespace SoluiNet.DevTools.Web
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using System.ServiceProcess;
     using System.Text;
     using System.Threading.Tasks;
@@ -28,7 +29,66 @@ namespace SoluiNet.DevTools.Web
                 new SoluiNetService(),
             };
 
-            ServiceBase.Run(servicesToRun);
+            // In interactive and debug mode ?
+            if (Environment.UserInteractive && System.Diagnostics.Debugger.IsAttached)
+            {
+                // Simulate the services execution
+                RunInteractiveServices(servicesToRun);
+            }
+            else
+            {
+                // Normal service execution
+                ServiceBase.Run(servicesToRun);
+            }
+        }
+
+        /// <summary>
+        /// Run services in interactive mode.
+        /// </summary>
+        private static void RunInteractiveServices(ServiceBase[] servicesToRun)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Start the services in interactive mode.");
+            Console.WriteLine();
+
+            // Get the method to invoke on each service to start it
+            MethodInfo onStartMethod = typeof(ServiceBase).GetMethod("OnStart", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Start services loop
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Starting {0} ... ", service.ServiceName);
+                onStartMethod.Invoke(service, new object[] { new string[] { } });
+                Console.WriteLine("Started");
+            }
+
+            // Waiting the end
+            Console.WriteLine();
+            Console.WriteLine("Press a key to stop services et finish process...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            // Get the method to invoke on each service to stop it
+            MethodInfo onStopMethod = typeof(ServiceBase).GetMethod("OnStop", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Stop loop
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Stopping {0} ... ", service.ServiceName);
+                onStopMethod.Invoke(service, null);
+                Console.WriteLine("Stopped");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("All services are stopped.");
+
+            // Waiting a key press to not return to VS directly
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine();
+                Console.Write("=== Press a key to quit ===");
+                Console.ReadKey();
+            }
         }
     }
 }
