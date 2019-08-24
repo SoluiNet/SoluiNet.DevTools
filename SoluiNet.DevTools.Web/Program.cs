307384fbd2b3b929957857f45769cf8e2e38a6e4
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-08-24 22:19:20 +0200

    Web - add windows server which should function as web server

---
+++ SoluiNet.DevTools.Web/Program.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Program.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.ServiceProcess;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The service program.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entrance of the application.
+        /// </summary>
+        public static void Main()
+        {
+            ServiceBase[] servicesToRun;
+
+            servicesToRun = new ServiceBase[]
+            {
+                new SoluiNetService(),
+            };
+
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
---
+++ SoluiNet.DevTools.Web/SoluiNetService.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="SoluiNetService.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.ServiceProcess;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The SoluiNet service.
+    /// </summary>
+    public partial class SoluiNetService : ServiceBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SoluiNetService"/> class.
+        /// </summary>
+        public SoluiNetService()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The start event handler.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// The stop event handler.
+        /// </summary>
+        protected override void OnStop()
+        {
+        }
+    }
+}
