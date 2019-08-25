bd491dc5033e2701a0a5c13addafabe61983e9e4
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-08-25 21:17:04 +0200

    Core.Web - add new core library providing web related methods

---
+++ SoluiNet.DevTools.Core.Web/Renderer/WebRenderer.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="WebRenderer.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.Web.Renderer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// The web renderer.
+    /// </summary>
+    public static class WebRenderer
+    {
+        /// <summary>
+        /// Render a page.
+        /// </summary>
+        /// <param name="rawMarkup">The raw markup.</param>
+        /// <returns>Returns the rendered markup as string.</returns>
+        public static string RenderPage(string rawMarkup)
+        {
+            return rawMarkup;
+        }
+    }
+}
