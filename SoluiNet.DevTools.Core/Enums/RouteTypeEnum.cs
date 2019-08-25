df870ad45f034ef94fa76c30c983c70ce9a5312b
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-08-25 18:22:12 +0200

    Core - add new route attribute

---
+++ SoluiNet.DevTools.Core/Enums/RouteTypeEnum.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="RouteTypeEnum.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.Enums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An enumeration of route types.
+    /// </summary>
+    public enum RouteTypeEnum
+    {
+        /// <summary>
+        /// A route which is based on controller.
+        /// </summary>
+        Controller = 1,
+
+        /// <summary>
+        /// A route which is based on action.
+        /// </summary>
+        Action = 2,
+
+        /// <summary>
+        /// A route which is based on controller and action.
+        /// </summary>
+        ControllerAndAction = 3,
+    }
+}
