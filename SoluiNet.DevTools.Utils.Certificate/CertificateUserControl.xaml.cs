d09fb57e3dde82bf70e798d401fa47d9e65fbc82
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2020-03-25 20:56:39 +0100

    Utils.Certificate - add new plugin
    Added new plugin which will support the user with certificate related tasks. At the moment this plugin will allow to get additional informations from a certificate which is hosted at an overgiven URL.

---
+++ SoluiNet.DevTools.Utils.Certificate/CertificateUserControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="CertificateUserControl.xaml.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Utils.Certificate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net;
+    using System.Security;
+    using System.Security.Cryptography;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for CertificateUserControl.xaml.
+    /// </summary>
+    public partial class CertificateUserControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CertificateUserControl"/> class.
+        /// </summary>
+        public CertificateUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GetInformation_Click(object sender, RoutedEventArgs e)
+        {
+            var url = this.CertificateUrl.Text;
+
+            var certificate = CertificateTools.GetCertificateByUrl(new Uri(url));
+
+            this.InformationBox.Text += string.Format(
+                CultureInfo.InvariantCulture,
+                "--- Information from '{4}' ---\r\nCN: {0}\r\nSubject: {1}\r\nCE Date: {2}\r\nPublicKey: {3}\r\n\r\n",
+                certificate.Issuer,
+                certificate.Subject,
+                certificate.GetExpirationDateString(),
+                certificate.GetPublicKeyString(),
+                url);
+        }
+    }
+}
---
+++ SoluiNet.DevTools.Utils.Certificate/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="AssemblyInfo.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("85c5b1d4-c258-403a-85cb-afe9be7e6be8")]
