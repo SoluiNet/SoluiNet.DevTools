30eead718251eafb671e56ba90e39eb2a508be85
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2020-04-15 20:05:49 +0200

    Utils.Json - add plugin for JSON object handling
    Added a new plugin which should support the user while working with JSON objects.

---
+++ SoluiNet.DevTools.Utils.Json/JsonTools.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="JsonTools.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Utils.Json
+{
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Reflection;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.CSharp;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using SoluiNet.DevTools.Core.Tools.String;
+
+    /// <summary>
+    /// Provides a collection of methods for JSON object handling.
+    /// </summary>
+    public static class JsonTools
+    {
+        /// <summary>
+        /// Formats a JSON string (taken from https://stackoverflow.com/questions/4580397/json-formatter-in-c).
+        /// </summary>
+        /// <param name="jsonString">The JSON string.</param>
+        /// <returns>A formatted JSON string.</returns>
+        public static string Format(string jsonString)
+        {
+            // Example 1
+            /* var t = "{\"x\":57,\"y\":57.0,\"z\":\"Yes\"}";
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(t);
+            var f = Newtonsoft.Json.JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(f); */
+
+            // Example 2
+            /* JToken jt = JToken.Parse(t);
+            string formatted = jt.ToString(Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(formatted); */
+
+            // Example 2 in one line
+            return JToken.Parse(jsonString).ToString(Formatting.Indented);
+        }
+    }
+}
---
+++ SoluiNet.DevTools.Utils.Json/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="AssemblyInfo.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("2eef3f90-8ebb-4171-bedc-4ea30d966867")]
