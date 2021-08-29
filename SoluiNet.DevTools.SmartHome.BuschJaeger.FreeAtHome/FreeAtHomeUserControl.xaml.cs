4c21b8d0fe92f0fd9766b338e571f07b9d6dc2b6
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2021-08-30 01:12:33 +0200

    add UI and integrate XMPP

---
+++ SoluiNet.DevTools.SmartHome.BuschJaeger.FreeAtHome/FreeAtHomeUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="FreeAtHomeUserControl.xaml.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.SmartHome.BuschJaeger.FreeAtHome
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for FreeAtHomeUserControl.xaml.
+    /// </summary>
+    public partial class FreeAtHomeUserControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FreeAtHomeUserControl"/> class.
+        /// </summary>
+        public FreeAtHomeUserControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
