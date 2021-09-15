0cb12d678117207b69c544011cdb2c1c56dd31a9
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2021-09-15 19:59:08 +0200

    adjust core storage library

--- SoluiNet.DevTools.Core.Storage/StorageRegistry.cs
+++ SoluiNet.DevTools.Core.Storage/StorageRegistry.cs
@@ -5,11 +5,19 @@
 namespace SoluiNet.DevTools.Core.Storage
 {
     using System;
+    using SoluiNet.DevTools.Core.Configuration;
 
     /// <summary>
     /// Provides a registry for all storage activities.
     /// </summary>
     public static class StorageRegistry
     {
+        /// <summary>
+        /// Gets the connection string.
+        /// </summary>
+        public static string ConnectionString
+        {
+            get { return Core.Configuration.Configuration.GetConfigurationEntry("StorageConnectionString"); }
+        }
     }
 }
