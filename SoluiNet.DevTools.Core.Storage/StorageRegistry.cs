39f6dab9c90db8e2df226e6d129be9b01473b409
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2021-09-15 19:51:55 +0200

    add storage core library

---
+++ SoluiNet.DevTools.Core.Storage/StorageRegistry.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="StorageRegistry.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.Storage
+{
+    using System;
+
+    /// <summary>
+    /// Provides a registry for all storage activities.
+    /// </summary>
+    public static class StorageRegistry
+    {
+    }
+}
