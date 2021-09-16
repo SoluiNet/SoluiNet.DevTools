d0a8cdf197fd52afa2279facd77555398f404790
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2021-09-16 13:59:13 +0200

    add new console project for faster setups

---
+++ SoluiNet.DevTools.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Console
+{
+    using System;
+
+    /// <summary>
+    /// The main entrance point for the SoluiNet.DevTools.Console application.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main method which should be called when executing this application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
