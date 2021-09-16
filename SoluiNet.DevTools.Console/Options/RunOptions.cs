d3d856c4177cf9d3fec3ef349547de4c2104fb90
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2021-09-16 14:53:20 +0200

    first implementation for console app

---
+++ SoluiNet.DevTools.Console/Options/RunOptions.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="RunOptions.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Console.Options
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CommandLine;
+
+    /// <summary>
+    /// The run options for the SoluiNet.DevTools.Console application.
+    /// </summary>
+    public class RunOptions
+    {
+        /// <summary>
+        /// Gets or sets whether the application should be verbose or not.
+        /// </summary>
+        [Option(
+            'v',
+            "verbose",
+            Default = false,
+            HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the application should be showing the help or not.
+        /// </summary>
+        [Option(
+            'h',
+            "help",
+            Default = false,
+            HelpText = "Show help information.")]
+        public bool Help { get; set; }
+    }
+}
--- SoluiNet.DevTools.Console/Program.cs
+++ SoluiNet.DevTools.Console/Program.cs
@@ -2,9 +2,15 @@
 // Copyright (c) SoluiNet. All rights reserved.
 // </copyright>
 
+using System.Reflection;
+
 namespace SoluiNet.DevTools.Console
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using CommandLine;
+    using SoluiNet.DevTools.Console.Options;
 
     /// <summary>
     /// The main entrance point for the SoluiNet.DevTools.Console application.
@@ -17,7 +23,35 @@ namespace SoluiNet.DevTools.Console
         /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLine.Parser.Default.ParseArguments<RunOptions>(args)
+                .WithParsed(Run)
+                .WithNotParsed(Error);
+        }
+
+        /// <summary>
+        /// Run with parsed options.
+        /// </summary>
+        /// <param name="options">The parsed options.</param>
+        internal static void Run(RunOptions options)
+        {
+            Console.WriteLine($@"SoluiNet.DevTools.Console v{Assembly.GetEntryAssembly()?.GetName().Version.ToString()}");
+            Console.WriteLine($@"Current Arguments: -v {options.Verbose} -h {options.Help}");
+
+            if (options.Help)
+            {
+                Console.WriteLine(@"You can use the following options:");
+                Console.WriteLine(@"v, verbose   Use verbose output");
+                Console.WriteLine(@"h, help      Open additional information about the usage of this application");
+            }
+        }
+
+        /// <summary>
+        /// Couldn't identify the options.
+        /// </summary>
+        /// <param name="errors">A enumerable which holds the errors.</param>
+        internal static void Error(IEnumerable<Error> errors)
+        {
+
         }
     }
 }
