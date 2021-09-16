00d3460de6baf0318219be1f55416d2e1f1cb6bd
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2021-09-16 17:18:59 +0200

    add mobile application

---
+++ SoluiNet.DevTools.Mobile/SoluiNet.DevTools.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SoluiNet.DevTools.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SoluiNet.DevTools.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
