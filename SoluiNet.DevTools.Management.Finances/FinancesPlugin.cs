0f99fb23092a8ae71ccac46d4b84cec8d0144b1e
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2021-12-19 18:12:09 +0100

    add finances plugin

---
+++ SoluiNet.DevTools.Management.Finances/FinancesPlugin.cs
@@ -0,0 +1,111 @@
+﻿using SoluiNet.DevTools.Core.Plugin;
+using SoluiNet.DevTools.Core.UI.WPF.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Security.Principal;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SoluiNet.DevTools.Management.Finances
+{
+    /// <summary>
+    /// Provides a plugin that allows one to manage finances.
+    /// </summary>
+    public class FinancesPlugin : IManagementPlugin, IManagementUiPlugin
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name
+        {
+            get { return "FinancesPlugin"; }
+        }
+
+        /// <summary>
+        /// Gets or sets the environment.
+        /// </summary>
+        public string Environment { get; set; }
+
+        /// <summary>
+        /// Gets the connection string name.
+        /// </summary>
+        public string ConnectionStringName { get; }
+
+        /// <summary>
+        /// Gets the default connection string name.
+        /// </summary>
+        public string DefaultConnectionStringName { get; }
+
+        /// <summary>
+        /// Gets or sets the first accent colour.
+        /// </summary>
+        public Color AccentColour1
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the second accent colour.
+        /// </summary>
+        public Color AccentColour2
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the foreground colour.
+        /// </summary>
+        public Color ForegroundColour
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the background colour.
+        /// </summary>
+        public Color BackgroundColour
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the background accent colour.
+        /// </summary>
+        public Color BackgroundAccentColour
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Display the plugin.
+        /// </summary>
+        /// <param name="mainGrid">The main grid.</param>
+        public void Display(Grid mainGrid)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Execute a SQL statement.
+        /// </summary>
+        /// <param name="sqlCommand">The SQL statement.</param>
+        /// <param name="identity">The windows identity under which the statement should be running.</param>
+        /// <returns>Returns a DataTable for the result.</returns>
+        public DataTable ExecuteSql(string sqlCommand, WindowsIdentity identity = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Execute a SQL script.
+        /// </summary>
+        /// <param name="sqlCommand">The SQL script.</param>
+        /// <param name="identity">The windows identity under which the script should be running.</param>
+        /// <returns>Returns a collection of DataTables for the result.</returns>
+        public ICollection<DataTable> ExecuteSqlScript(string sqlCommand, WindowsIdentity identity = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SoluiNet.DevTools.Management.Finances/FinancesUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="FinancesUserControl.xaml.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Management.Finances
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for FinancesUserControl.xaml.
+    /// </summary>
+    public partial class FinancesUserControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FinancesUserControl"/> class.
+        /// </summary>
+        public FinancesUserControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
