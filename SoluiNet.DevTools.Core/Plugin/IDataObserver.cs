b66e274d8ae78ae3f78721e24db3a2781376245c
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2022-01-06 23:27:31 +0100

    add observer plugin interface

---
+++ SoluiNet.DevTools.Core/Plugin/IDataObserver.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IDataObserver.cs" company="SoluiNet">
+// Copyright (c) SoluiNet. All rights reserved.
+// </copyright>
+
+namespace SoluiNet.DevTools.Core.Plugin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Provides an interface for observing data.
+    /// </summary>
+    public interface IDataObserver : IBasePlugin
+    {
+        /// <summary>
+        /// Subscribe to the data observer.
+        /// </summary>
+        /// <param name="observer">The observer.</param>
+        void Subscribe(IDataObserver observer);
+    }
+}
