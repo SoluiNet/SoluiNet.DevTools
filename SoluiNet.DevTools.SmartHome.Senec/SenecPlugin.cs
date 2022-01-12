9faa99963cdf3dc2d240771a7ca1928b2bd6065b
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2022-01-12 22:57:16 +0100

    improve SENEC plugin

--- SoluiNet.DevTools.SmartHome.Senec/SenecPlugin.cs
+++ SoluiNet.DevTools.SmartHome.Senec/SenecPlugin.cs
@@ -20,6 +20,7 @@ namespace SoluiNet.DevTools.SmartHome.Senec
     using SoluiNet.DevTools.Core.Plugin;
     using SoluiNet.DevTools.Core.SmartHome.Data;
     using SoluiNet.DevTools.Core.Tools.Number;
+    using SoluiNet.DevTools.Core.UI.WPF.Extensions;
     using SoluiNet.DevTools.Core.UI.WPF.Plugin;
 
     /// <summary>
@@ -112,12 +113,51 @@ namespace SoluiNet.DevTools.SmartHome.Senec
         }
 
         /// <summary>
+        /// Gets or sets the main grid.
+        /// </summary>
+        public Grid MainGrid { get; set; }
+
+        /// <summary>
         /// Display the plugin.
         /// </summary>
         /// <param name="mainGrid">The main grid.</param>
         public void Display(Grid mainGrid)
         {
-            throw new NotImplementedException();
+            this.MainGrid = mainGrid;
+
+            var tabControl = mainGrid.GetChildOfType<TabControl>();
+
+            if (tabControl.Name == "MainTabs")
+            {
+                var tabItem = new TabItem()
+                {
+                    Header = "SENEC",
+                    Name = "Senec_TabItem",
+                    Background = new LinearGradientBrush(this.AccentColour1, this.AccentColour2, 0.00),
+                    Foreground = new SolidColorBrush(this.ForegroundColour),
+                };
+
+                tabControl.SelectionChanged += (sender, eventArgs) =>
+                {
+                    if (eventArgs.Source is TabControl)
+                    {
+                        if (tabItem.IsSelected)
+                        {
+                            tabControl.Background = new SolidColorBrush(this.BackgroundColour);
+                        }
+                    }
+                };
+
+                tabControl.Items.Add(tabItem);
+
+                tabItem.Content = new Grid()
+                {
+                    Name = "Senec_TabItem_Content",
+                    Background = new LinearGradientBrush(this.BackgroundAccentColour, this.BackgroundColour, 45.00),
+                };
+
+                ((Grid)tabItem.Content).Children.Add(new SenecUserControl());
+            }
         }
 
         /// <summary>
